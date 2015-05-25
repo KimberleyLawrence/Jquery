8848f1a2800acf03c5762fe22a1e7e6aa424f810
KimberleyLawrence <kimberleynedic@gmail.com>
2015-05-25 13:51:28 -0700

    Syntax, Getting started
    See notes in .js

---
+++ syntax_getting_started.js
@@ -0,0 +1,7 @@
+$(document).ready();
+
+// $() = tells jQuery that things are about to happen//
+// document = between the () tells us that we are about to work our magic on the html document itself.//
+// .ready = is a function, or basic action, in jQuery. It's saying that as soon as the html doc is ready it will do things.//
+// .ready() = whatever goes into .ready's () is the jQuery event that occurs as soon as the html doc is ready.//
+// ; = tells jQuery that you are done giving a command. //
