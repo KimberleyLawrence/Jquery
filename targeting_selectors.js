2afeca258a258426f20dfffe0ae446f4a0239f65
KimberleyLawrence <kimberleynedic@gmail.com>
2015-05-25 13:07:30 -0700

    Targeting selectors
    See notes in .js but you can target selectors just like you would with
    css, just remember the quotation marks

---
+++ targeting_selectors.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('#green').fadeOut(1000);
+});
+// just like in css, you can target an element/class/id in the html doc, think of the element name passed to jQuery as identical to the CSS elelment only wrapped in '' quotes. You can target class="button" exactly how you would in CSS, but with quotation marks = ".button". Above is if you were targeting the id="green". //
