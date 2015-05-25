e12f9e8bb3e59ca5a2210578d4c61e71c20eea33
KimberleyLawrence <kimberleynedic@gmail.com>
2015-05-25 14:20:43 -0700

    Functions
    See notes in .js code

---
+++ function.js
@@ -0,0 +1,9 @@
+$(document).ready(function{
+    });
+// function = pretty much is action. They are the basic unit of doing work in jQuery//
+// the jQuery syntax for function looks like;//
+// function(){ //
+//    jQuery magic; //
+// } //
+// but the above function syntax(in notes) will do nothing unless it is called, like the code on line one//
+// the code on line one will make our function run as soon as the html document loads//
