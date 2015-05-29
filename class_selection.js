f281299b2cae27c6dbd92f3536618855daf08a79
KimberleyLawrence <kimberleynedic@gmail.com>
2015-05-28 23:14:42 -0700

    Class selection
    We don't have to limit ourselves to selecting HTML elements like <p>
    and <div>; essentially, we can put any CSS selector in quotes and pass
    it into $(). This means we can select classes, too!
    
    Recall that we can select classes in CSS by using a dot (.). If we have
    class="red" in our HTML, we can target it in CSS with .red. In jQuery,
    all we need to do is put '.red' in quotes, and we can pass it to $() to
    make a jQuery object.

---
+++ class_selection.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.vanish').fadeOut('slow');
+        
+    });
+});
+// there is a button that says click here, that is the button we are targeting to set off the action//
