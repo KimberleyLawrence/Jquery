6ce92bed985950758036e9129acd7fb052198eff
KimberleyLawrence <kimberleynedic@gmail.com>
2015-07-06 12:19:31 -0700

    Accordion
    See notes in code,
    using the accordion function to make the sections into click down boxes

---
+++ accordion_2.js
@@ -0,0 +1,4 @@
+$(document).ready(function (){
+    $('#menu').accordion({collapsible:true, active:false});
+});
+///collapsible means that the sections will collapse to open up and show more details ///
